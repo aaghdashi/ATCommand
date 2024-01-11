59ef3cb22bd873b47f5dbfed8bbb8594711f71c1
Arman Aghdashi <arman.aghdashy@gmail.com>
2024-01-11 13:14:52 -0800

    Delete sms directory

--- sms/C# receive SMS_files/count.js
+++
@@ -1,8 +0,0 @@
-// statcounter
-document.write("<script type=\"text\/javascript\">");
-document.write("var sc_project=6906282; ");
-document.write("var sc_invisible=1; ");
-document.write("var sc_security=\"79d36d97\"; ");
-document.write("<\/script>");
-document.write("<script type=\"text\/javascript\"");
-document.write("src=\"http:\/\/www.statcounter.com\/counter\/counter.js\"><\/script>");
